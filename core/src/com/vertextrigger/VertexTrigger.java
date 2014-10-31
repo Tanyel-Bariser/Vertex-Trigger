c15a0a9f860e05b7884651c0063bfc5bc426fc00
Tanyel Bariser <tanyel19@hotmail-com>
2014-10-31 01:37:59 +0000

    Javadoc, method signature & comments for Vertex Trigger class

--- core/src/com/vertextrigger/VertexTrigger.java
+++ core/src/com/vertextrigger/VertexTrigger.java
@@ -1,27 +1,18 @@
 package com.vertextrigger;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * First class called by the respective port's, i.e. Android, Desktop,
+ * HTML, bootstrapping class.
+ */
 public class VertexTrigger extends Game {
-	SpriteBatch batch;
-	Texture img;
 	
+	/**
+	 * First method called when Vertex Trigger is first created
+	 */
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		// Set screen to main menu screen
 	}
 }
