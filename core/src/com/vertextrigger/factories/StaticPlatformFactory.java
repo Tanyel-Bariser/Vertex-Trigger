181fab3ebdd58bc758e424d648d99b0e7e3a1a00
Tanyel Bariser <tanyel19@hotmail-com>
2014-11-12 15:19:58 +0000

    Refactored StaticPlatfromFactory methods into a single one.

--- core/src/com/vertextrigger/factories/StaticPlatformFactory.java
+++ core/src/com/vertextrigger/factories/StaticPlatformFactory.java
@@ -2,59 +2,29 @@ package com.vertextrigger.factories;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+import com.vertextrigger.Coordinate;
+import com.vertextrigger.entities.Entity;
 
 /**
- *
+ * PERHAPS INCLUDE CREATION FOR ALL TYPES OF PLATFORM!!!
  */
-public class StaticPlatformFactory {
-
-	/**
-	 * @param world
-	 * @param name
-	 * @param spriteWidth
-	 * @param spriteHeight
-	 * @param bodyWidth
-	 * @param bodyHeight
-	 * @return
-	 */
-	public static Sprite createStickyPlatform(World world, String name,
-			float spriteWidth, float spriteHeight, float bodyWidth,
-			float bodyHeight) {
-		// Set identifier label of fixture to "StickyPlatform"
-		return null;
-	}
+public class StaticPlatformFactory {	
 	
-
 	/**
-	 * @param world
-	 * @param name
+	 * @param world platform resides in
+	 * @param spriteName name of platform sprite in texture atlas
+	 * @param userData is the identifier label of platform
 	 * @param spriteWidth
 	 * @param spriteHeight
 	 * @param bodyWidth
 	 * @param bodyHeight
-	 * @return
+	 * @return platform's sprite
 	 */
-	public static Sprite createSlipperyPlatform(World world, String name,
+	public static Sprite createStaticPlatform(World world, String spriteName, String userData,
 			float spriteWidth, float spriteHeight, float bodyWidth,
 			float bodyHeight) {
-		// Set identifier label of fixture to "SlipperyPlatform"
-		return null;
-	}
-	
-
-	/**
-	 * @param world
-	 * @param name
-	 * @param spriteWidth
-	 * @param spriteHeight
-	 * @param bodyWidth
-	 * @param bodyHeight
-	 * @return
-	 */
-	public static Sprite createBouncyPlatform(World world, String name,
-			float spriteWidth, float spriteHeight, float bodyWidth,
-			float bodyHeight) {
-		// Set identifier label of fixture to "BouncyPlatform"
+		// Set identifier label of fixture as the platform's userData
 		return null;
 	}
 }
