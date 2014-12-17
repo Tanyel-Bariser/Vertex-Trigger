5189af2365c095d602d54c2af5d7c2a3fc3db17d
hughjdavey <hughjd@googlemail.com>
2014-12-17 23:24:01 +0000

    generified loading level assets code; need to add a method that creates a set of descriptors for each level

--- core/src/com/vertextrigger/Assets.java
+++ core/src/com/vertextrigger/Assets.java
@@ -1,9 +1,19 @@
 package com.vertextrigger;
 
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetManager;
+
+import java.util.List;
+import java.util.Set;
+
 /**
  * Loads & stores assets, i.e. textures, bitmapfonts, sound effects, music, etc.
  */
 public class Assets {
+
+    private static AssetManager assetManager;
+
+    private static List<Set<AssetDescriptor<?>>> levelAssets;
 
 	/**
 	 * Initialise AssetManager before using this class, to be used for the rest
@@ -12,7 +22,10 @@ public class Assets {
 	 * for each screen/level.
 	 */
 	public static void initialiseAssetManager() {
-		// Create asset manager
+		// Create asset manager only if one does not already exist
+        if (assetManager == null) {
+            assetManager = new AssetManager();
+        }
 		// Create container of main menu assets to be loaded
 		// Create container of all assets not in main menu to be unloaded
 		// Create container of level one assets to be loaded
@@ -32,29 +45,38 @@ public class Assets {
 	}
 
 	/**
-	 * Loads assets for LevelOneScreen
+	 * Loads assets for a level screen
 	 */
-	public static void loadLevelOneAssets() {
-		// For each level one asset
-				// Load asset to be stored in asset manager
-		// For each asset not in level one
-				// Unload asset from asset manager
+	public static void loadLevelAssets(int level) {
+		// for each level asset we load the asset to be stored in asset manager
+		// for each asset not in level we unload the asset from the asset manager
+        // the for loop iterates through levelAssets, a list of sets of level assets
+
+        for (int i = 1; i <= levelAssets.size(); i++) {
+            if (i == level) {
+                for (AssetDescriptor<?> asset : levelAssets.get(i)) {
+                    assetManager.load(asset);
+                }
+            }
+            else {
+                for (AssetDescriptor<?> asset : levelAssets.get(i)) {
+                    assetManager.unload(asset.fileName);
+                }
+            }
+        }
 	}
 
-	/**
-	 * Loads assets for LevelTwoScreen
-	 */
-	public static void loadLevelTwoAssets() {
-		// For each level two asset
-				// Load asset to be stored in asset manager
-		// For each asset not in level two
-				// Unload asset from asset manager
-	}
+    /**
+     * Loads assets for the Heads-Up Display and the Controls for each level
+     */
+    private static void loadHudAndControls() {
+    }
 
 	/**
 	 * Dispose of assets once they're no longer needed
 	 */
 	public static void dispose() {
-		// Release all assets and asset manager
+		// Release all assets and asset manager itself
+        assetManager.dispose();
 	}
 }
