c9c1f8acf9ce51706c6af3d9c867286aa8a7ff0c
hughjdavey <hughjd@googlemail.com>
2014-12-17 23:33:34 +0000

    added some pseudocode to demonstrate asset loading - will fill in when we have decided exactly which assets go in each collection

--- core/src/com/vertextrigger/Assets.java
+++ core/src/com/vertextrigger/Assets.java
@@ -3,6 +3,8 @@ package com.vertextrigger;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetManager;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -13,13 +15,13 @@ public class Assets {
 
     private static AssetManager assetManager;
 
+    private static HashSet<AssetDescriptor<?>> levelOneAssets, levelTwoAssets, menuScreenAssets, hudAndControlAssets;
+
     private static List<Set<AssetDescriptor<?>>> levelAssets;
 
 	/**
-	 * Initialise AssetManager before using this class, to be used for the rest
-	 * of them game.
-	 * Initialise containers of assets to be loaded or unloaded
-	 * for each screen/level.
+	 * Initialise AssetManager before using this class, to be used for the rest of the game
+	 * Initialise containers of assets to be loaded or unloaded for each screen/level
 	 */
 	public static void initialiseAssetManager() {
 		// Create asset manager only if one does not already exist
@@ -32,6 +34,16 @@ public class Assets {
 		// Create container of all assets not in level one to be unloaded
 		// Create container of level two assets to be loaded
 		// Create container of all assets not in level two to be unloaded
+
+        levelAssets = new ArrayList<Set<AssetDescriptor<?>>>();
+
+        levelOneAssets = new HashSet<AssetDescriptor<?>>();
+        // add level one assets to hashset
+        levelTwoAssets = new HashSet<AssetDescriptor<?>>();
+        // add level two assets to hashset
+
+        levelAssets.add(levelOneAssets);
+        levelAssets.add(levelTwoAssets);
 	}
 
 	/**
@@ -44,12 +56,14 @@ public class Assets {
 				// Unload asset from asset manager
 	}
 
-	/**
-	 * Loads assets for a level screen
-	 */
+    /**
+     * Loads assets for a particular level
+     *
+     * @param level the number of the level assets we wish to load
+     */
 	public static void loadLevelAssets(int level) {
-		// for each level asset we load the asset to be stored in asset manager
-		// for each asset not in level we unload the asset from the asset manager
+		// for each level asset we load the assets to be stored in asset manager
+		// for each asset not in level we unload the assets from the asset manager
         // the for loop iterates through levelAssets, a list of sets of level assets
 
         for (int i = 1; i <= levelAssets.size(); i++) {
@@ -70,6 +84,9 @@ public class Assets {
      * Loads assets for the Heads-Up Display and the Controls for each level
      */
     private static void loadHudAndControls() {
+        for (AssetDescriptor<?> asset : hudAndControlAssets) {
+            assetManager.load(asset);
+        }
     }
 
 	/**
