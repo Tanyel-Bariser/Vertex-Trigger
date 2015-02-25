5c382a20852db100fb77808e985666a76cb39797
hughjdavey <hughjd@googlemail.com>
2015-02-25 22:18:38 +0000

    added some code lost in the last pull

--- core/src/Main/com/vertextrigger/util/Assets.java
+++ core/src/Main/com/vertextrigger/util/Assets.java
@@ -2,22 +2,24 @@ package com.vertextrigger.util;
 
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 /**
  * Loads & stores assets, i.e. textures, bitmapfonts, sound effects, music, etc.
  */
 public class Assets {
+    final public static int LEVEL_ONE = 1;
+    final public static int LEVEL_TWO = 2;
+    // etc
 
     private static AssetManager assetManager;
-
-    private static HashSet<AssetDescriptor<?>> levelOneAssets, levelTwoAssets, menuScreenAssets, hudAndControlAssets;
-
-    private static List<Set<AssetDescriptor<?>>> levelAssets;
+    private static Map<Integer, AssetDescriptor<TextureAtlas>> levelTextures;
+    private static List<AssetDescriptor<?>> music, sfx, hud, mainScreen, font;
 
 	/**
 	 * Initialise AssetManager before using this class, to be used for the rest of the game
@@ -28,22 +30,11 @@ public class Assets {
         if (assetManager == null) {
             assetManager = new AssetManager();
         }
-		// Create container of main menu assets to be loaded
-		// Create container of all assets not in main menu to be unloaded
-		// Create container of level one assets to be loaded
-		// Create container of all assets not in level one to be unloaded
-		// Create container of level two assets to be loaded
-		// Create container of all assets not in level two to be unloaded
 
-        levelAssets = new ArrayList<Set<AssetDescriptor<?>>>();
+        levelTextures = new HashMap<>();
+        //levelTextures.add(AssetFilenames.LEVEL_ONE_ATLAS);
+        //levelTextures.add(AssetFilenames.LEVEL_TWO_ATLAS);
 
-        levelOneAssets = new HashSet<AssetDescriptor<?>>();
-        // add level one assets to hashset
-        levelTwoAssets = new HashSet<AssetDescriptor<?>>();
-        // add level two assets to hashset
-
-        levelAssets.add(levelOneAssets);
-        levelAssets.add(levelTwoAssets);
 	}
 
 	/**
@@ -52,10 +43,8 @@ public class Assets {
 	public static void loadMainScreenAssets() {
 		// for each main menu asset we load the asset to be stored in asset manager
 		// for each asset not in main menu we unload asset from the asset manager
-        dispose();
-
-        for (AssetDescriptor<?> asset : menuScreenAssets) {
-            assetManager.load(asset);
+        for (AssetDescriptor<?> mainScreenAsset : mainScreen) {
+            assetManager.load(mainScreenAsset);
         }
 	}
 
@@ -65,31 +54,11 @@ public class Assets {
      * @param level the number of the level assets we wish to load
      */
 	public static void loadLevelAssets(int level) {
-		// for each level asset we load the assets to be stored in asset manager
-		// for each asset not in level we unload the assets from the asset manager
-        // the for loop iterates through levelAssets, a list of sets of level assets
-        dispose();
+		// unload currently loaded assets
+        assetManager.clear();
 
-        level = --level;   // subtract one from level to align with the 0-indexed arraylist collection
-        for (int i = 0; i <= levelAssets.size(); i++) {
-            if (i == level) {
-                for (AssetDescriptor<?> asset : levelAssets.get(i)) {
-                    assetManager.load(asset);
-                }
-            }
-        }
+        assetManager.load(levelTextures.get(level));
 	}
-
-    /**
-     * Loads assets for the Heads-Up Display and the Controls for each level
-     */
-    private static void loadHudAndControls() {
-        // for each hud and control asset we load the assets to be stored in asset manager
-        // we do not dispose of other assets as the hud/controls go on top of a level
-        for (AssetDescriptor<?> asset : hudAndControlAssets) {
-            assetManager.load(asset);
-        }
-    }
 
 	/**
 	 * Dispose of assets once they're no longer needed
