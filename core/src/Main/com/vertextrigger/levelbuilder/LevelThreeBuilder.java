af4301c3330e4f5fb3c2cc0dc8e55e6e3ebb0342
hughjdavey <hughjd@googlemail.com>
2016-01-24 16:17:49 +0000

    make levelbuilders implement create portals method correctly (was causing compile time crash)

--- core/src/Main/com/vertextrigger/levelbuilder/LevelThreeBuilder.java
+++ core/src/Main/com/vertextrigger/levelbuilder/LevelThreeBuilder.java
@@ -2,6 +2,8 @@ package com.vertextrigger.levelbuilder;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+import com.vertextrigger.inanimate.portal.Portal;
 import com.vertextrigger.screen.AbstractGameScreen;
 
 public class LevelThreeBuilder extends AbstractLevelBuilder {
@@ -47,6 +49,7 @@ public class LevelThreeBuilder extends AbstractLevelBuilder {
 	}
 
 	@Override
-	protected void createPortals() {
+	public Array<Portal> createPortals() {
+		return null;
 	}
 }
