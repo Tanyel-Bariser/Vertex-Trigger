8c465e8df8f7503c8301c181f8a8d09f92be427e
Tanyel Bariser <tanyel19@hotmail-com>
2017-07-16 22:52:41 +0100

    Added initial slider to replace left and right buttons in Android version

--- core/Main/com/vertextrigger/controller/AndroidController.java
+++ core/Main/com/vertextrigger/controller/AndroidController.java
@@ -4,10 +4,12 @@ import com.badlogic.gdx.*;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider.SliderStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.*;
 import com.badlogic.gdx.utils.Align;
 import com.vertextrigger.entities.player.Player;
 import com.vertextrigger.factory.ButtonFactory;
+import com.vertextrigger.main.VertexTrigger;
 
 public class AndroidController extends Controller {
 	private final Table buttonLayer = new Table().debug();
@@ -20,8 +22,9 @@ public class AndroidController extends Controller {
 	public AndroidController(final Player player, final Screen screen, final Stage stage) {
 		super(player, screen);
 
-		createLeftButton(new ImageButton(ButtonFactory.getLeftButton()));
-		createRightButton(new ImageButton(ButtonFactory.getRightButton()));
+		createSlider();
+		// createLeftButton(new ImageButton(ButtonFactory.getLeftButton()));
+		// createRightButton(new ImageButton(ButtonFactory.getRightButton()));
 		createPauseButton(new ImageButton(ButtonFactory.getPauseButton(), null, ButtonFactory.getResumeButton()));
 		createShootButton(new ImageButton(ButtonFactory.getShootButton()));
 		createJumpButton(new ImageButton(ButtonFactory.getJumpButton()));
@@ -30,6 +33,44 @@ public class AndroidController extends Controller {
 		buttonLayer.setPosition(0, 0);
 		buttonLayer.align(Align.center | Align.bottom);
 		stage.addActor(buttonLayer);
+	}
+
+	private void createSlider() {
+		final int min = 0;
+		final int max = 10;
+		final float stepSize = 1.0f;
+		final boolean vertical = false;
+		final Skin skin = VertexTrigger.ASSETS.getCoreSkin();
+		final Drawable sliderImage = skin.getDrawable("magnetInactive");
+		final Drawable knobImage = skin.getDrawable("bullet");
+		final SliderStyle sliderStyle = new SliderStyle(sliderImage, knobImage);
+		final Slider slider = new Slider(min, max, stepSize, vertical, sliderStyle);
+		slider.setPosition(leftButtonPosition.x, leftButtonPosition.y);
+		final int width = 400;
+		final int middle = width / 2;
+		slider.addListener(new DragListener() {
+			@Override
+			public void touchDragged(final InputEvent event, final float x, final float y, final int pointer) {
+				if (isControllable()) {
+					if (x < middle) {
+						player.moveLeft();
+					} else {
+						player.moveRight();
+					}
+				}
+			}
+
+			@Override
+			public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer, final int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(final InputEvent event, final float x, final float y, final int pointer, final int button) {
+				player.stopMoving();
+			}
+		});
+		buttonLayer.add(slider).width(width).height(200);
 	}
 
 	private void createLeftButton(final ImageButton left) {
